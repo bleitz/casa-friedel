caf48f602a60ae76c74d2ea2c2abd6d87fc1403d
bleitz <39960235+bleitz@users.noreply.github.com>
2023-01-23 16:26:47 +0100

    Replaces grid with flex box

--- nav.js
+++ nav.js
@@ -31,3 +31,18 @@ window.addEventListener('scroll',(e)=>{
         nav.classList.remove("nav-shadow")
     }, 1500);
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("open");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
